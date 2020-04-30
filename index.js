abc82919c39f35ac409cbf6f9b50a56ef75b3653
tzztzsts <tzztzsts@gmail.com>
2020-04-30 18:47:17 +0900

    Improve

--- index.js
+++ index.js
@@ -44,13 +44,12 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
 
     //ユーザーからのテキストメッセージが想定していた文字列を含む場合のみ反応
     if (messageObj.word_list.some(value => event.message.text.match(value))){
-        //replyMessage()で返信し、そのプロミスをevents_processedに追加
-        events_processed.push(bot.replyMessage(event.replyToken, {
-          /*type: "flex",
-          altText: "席替えの結果",
-          contents: flexMessage()*/
-          type: "text",
-          text: "abc"
+      const sendMessage = JSON.stringify(flexMessage())
+      //replyMessage()で返信し、そのプロミスをevents_processedに追加
+      events_processed.push(bot.replyMessage(event.replyToken, {
+        type: "flex",
+        altText: "席替えの結果",
+        contents: sendMessage
       }));
     };
   });
