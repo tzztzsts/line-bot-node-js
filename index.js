e5e6e15770320b9f9dfb1be81e6737a94f8cc0c9
tzztzsts <tzztzsts@gmail.com>
2020-05-01 00:25:49 +0900

    Improve

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 //モジュールのインポート
 const fs = require('fs');
+const server = require("express")();
+const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
+
+// -----------------------------------------------------------------------------
+// パラメータ設定
+const line_config = {
+  channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセット
+  channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセット
+};
+
+// -----------------------------------------------------------------------------
+// Webサーバー設定
+server.listen(process.env.PORT || 3000);
+
+// -----------------------------------------------------------------------------
+// APIコールのためのクライアントインスタンスを作成
+const bot = new line.Client(line_config);
+
+// -----------------------------------------------------------------------------
+// ルーター設定
+server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
+
+  res.sendStatus(200);// 先行してLINE側にステータスコード200でレスポンスする
 
 //想定済文字烈の読み込み
 const jsonText = fs.readFileSync('./again-message.json', 'utf8', (error, data) => {
--- report.js
+++ report.js
@@ -8,26 +8,7 @@ const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
 
 const changed = require("./change");
 
-// -----------------------------------------------------------------------------
-// パラメータ設定
-const line_config = {
-  channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセット
-  channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセット
-};
 
-// -----------------------------------------------------------------------------
-// Webサーバー設定
-server.listen(process.env.PORT || 3000);
-
-// -----------------------------------------------------------------------------
-// APIコールのためのクライアントインスタンスを作成
-const bot = new line.Client(line_config);
-
-// -----------------------------------------------------------------------------
-// ルーター設定
-server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
-
-  res.sendStatus(200);// 先行してLINE側にステータスコード200でレスポンスする
 
   let events_processed = [];// すべてのイベント処理のプロミスを格納する配列
 
