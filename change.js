cd32a9a7f0a2eb5c0457a8e11fb5a6da6bd69ac9
tzztzsts <tzztzsts@gmail.com>
2020-04-30 14:37:07 +0900

    Improve

---
+++ change.js
@@ -0,0 +1,56 @@
+//目が悪くない人たちの席次をまずはシャッフル
+const temporarySeats1 = [1, 2, 3, 4, 5, 6, 7, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 23, 24, 26, 27, 28, 30, 31, 32, 34, 35, 36, 37, 38];
+
+for (let i1 = temporarySeats1.length - 1; i1 >= 0; i1--) {
+  const j1 = Math.floor(Math.random() * (i1 + 1));
+  [temporarySeats1[i1], temporarySeats1[j1]] = [temporarySeats1[j1], temporarySeats1[i1]];
+};
+
+// -----------------------------------------------------------------------------
+//目が悪い人の席をランダムに８つ選ぶ。前から差し込まないとずれるので昇順でソート
+const array = [1, 2, 3, 8, 9, 10, 15, 16, 17, 21, 22, 23, 27, 28, 29, 34, 35, 36];
+const selected = [...Array(8)].map(() => array.splice(Math.floor(Math.random() * array.length), 1)[0]);
+
+function compareFunc(a, b) {
+  return a - b;
+};
+
+selected.sort(compareFunc);
+
+// -----------------------------------------------------------------------------
+//このままだと席次によって偏りが出るので目が悪い人の席次をシャッフル。さっきの席に当てはめるつもりで
+const temporarySeats2 = [8, 9, 10, 22, 25, 29, 33, 39];
+
+for (let i2 = temporarySeats2.length - 1; i2 >= 0; i2--) {
+  const j2 = Math.floor(Math.random() * (i2 + 1));
+  [temporarySeats2[i2], temporarySeats2[j2]] = [temporarySeats2[j2], temporarySeats2[i2]];
+};
+
+// -----------------------------------------------------------------------------
+//２つのtemporarySeatsが合わさって席が決定。なんかもっとシンプルな方法ありそう
+const changedSeats = (temporarySeats1, selected, temporarySeats2) => {
+  for (let i = 0; i < 8; i++) {
+    temporarySeats1.splice(selected[i] - 1, 0, temporarySeats2[i]);
+  };
+  return temporarySeats1;
+};
+
+changedSeats(temporarySeats1, selected, temporarySeats2);//席替え完了
+
+// -----------------------------------------------------------------------------
+//一斉送信用のflex messageをjsonファイルから読み込み、オブジェクトとして扱う
+const fs = require('fs');
+const json = fs.readFileSync('./flex-message', 'utf8');
+let flexMessageObj = JSON.parse(json);
+
+// -----------------------------------------------------------------------------
+//席替えの結果と日付に合わせてflex messageを書き換え。ついでにモジュール化
+module.exprts = function(date){
+
+  flexMessageObj.replace("{{day}}", date);
+
+  for (i = 1; i <= 39; i++) {
+    flexMessageObj.replace("{{number}}", "" + changedSeats[i - 1]);
+  }
+  return flexMessageObj;
+};
--- index.js
+++ index.js
@@ -1,60 +1,12 @@
+const flexMessage = require('./change')//change.jsのfunctionをインポート
+
+// -----------------------------------------------------------------------------
 //今日の日付を取得。前日に席替えするなら日付＋１
 const today = new Date();
 const date = today.getDate() + "";//Stringで扱う
 
 // -----------------------------------------------------------------------------
-//目が悪くない人たちの席次をまずはシャッフル
-const temporarySeats1 = [1, 2, 3, 4, 5, 6, 7, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 23, 24, 26, 27, 28, 30, 31, 32, 34, 35, 36, 37, 38];
 
-for (let i1 = temporarySeats1.length - 1; i1 >= 0; i1--) {
-  const j1 = Math.floor(Math.random() * (i1 + 1));
-  [temporarySeats1[i1], temporarySeats1[j1]] = [temporarySeats1[j1], temporarySeats1[i1]];
-};
-
-// -----------------------------------------------------------------------------
-//目が悪い人の席をランダムに８つ選ぶ。前から差し込まないとずれるので昇順でソート
-const array = [1, 2, 3, 8, 9, 10, 15, 16, 17, 21, 22, 23, 27, 28, 29, 34, 35, 36];
-const selected = [...Array(8)].map(() => array.splice(Math.floor(Math.random() * array.length), 1)[0]);
-
-function compareFunc(a, b) {
-  return a - b;
-};
-
-selected.sort(compareFunc);
-
-// -----------------------------------------------------------------------------
-//このままだと席次によって偏りが出るので目が悪い人の席次をシャッフル。さっきの席に当てはめるつもりで
-const temporarySeats2 = [8, 9, 10, 22, 25, 29, 33, 39];
-
-for (let i2 = temporarySeats2.length - 1; i2 >= 0; i2--) {
-  const j2 = Math.floor(Math.random() * (i2 + 1));
-  [temporarySeats2[i2], temporarySeats2[j2]] = [temporarySeats2[j2], temporarySeats2[i2]];
-};
-
-// -----------------------------------------------------------------------------
-//２つのtemporarySeatsが合わさって席が決定。なんかもっとシンプルな方法ありそう
-const changedSeats = (temporarySeats1, selected, temporarySeats2) => {
-  for (let i = 0; i < 8; i++) {
-    temporarySeats1.splice(selected[i] - 1, 0, temporarySeats2[i])
-  };
-  return temporarySeats1;
-};
-
-changedSeats(temporarySeats1, selected, temporarySeats2);//席替え完了
-
-// -----------------------------------------------------------------------------
-//一斉送信用のflex messageをjsonファイルから読み込み、オブジェクトとして扱う
-const fs = require('fs');
-const json = fs.readFileSync('http://seatchange.herokuapp.com/flex-message.json', 'utf8');
-let flexMessageObj = JSON.parse(json);
-
-// -----------------------------------------------------------------------------
-//席替えの結果と日付に合わせてflex messageを書き換え
-flexMessageObj.replace("{{day}}", date);
-
-for (i = 1; i <= 39; i++) {
-  flexMessageObj.replace("{{number}}", "" + changedSeats[i - 1]);
-};
 
 // -----------------------------------------------------------------------------
 // モジュールのインポート
@@ -84,17 +36,16 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
 
   let events_processed = [];// すべてのイベント処理のプロミスを格納する配列
 
-  const jsonText = fs.readFile('http://seatchange.herokuapp.com/again-message.json', 'utf8');
+  const jsonText = fs.readFile('./again-message', 'utf8');
   const messageObj = JSON.parse(jsonText);
 
     // イベントオブジェクトを順次処理。
   req.body.events.forEach((event) => {
 
-    if (event.type == "message" && event.message.type == "text"){// この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定
+    if (event.type === "message" && event.message.type === "text"){// この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定
 
-      if (messageObj.some(value => value == event.message.text){// ユーザーからのテキストメッセージが想定していたもの(再度座席表を送る)だった場合のみ反応
-
-        events_processed.push(bot.replyMessage(event.replyToken, flexMessageObj));// replyMessage()で返信し、そのプロミスをevents_processedに追加
+      if (messageObj.some(value => value === event.message.text){// ユーザーからのテキストメッセージが想定していたもの(再度座席表を送る)だった場合のみ反応
+        events_processed.push(bot.replyMessage(event.replyToken, flexMessage(date)));// replyMessage()で返信し、そのプロミスをevents_processedに追加
       }
     }
   })
