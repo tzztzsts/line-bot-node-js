d154fe7edda5ece021c0e28a05a1ca4b4b5f1871
tzztzsts <tzztzsts@gmail.com>
2020-04-30 09:15:29 +0900

    Improve

--- index.js
+++ index.js
@@ -1,38 +1,69 @@
+//今日の日付を取得
 const today = new Date();
+
+//前の日に席替えして日付＋１するかも
 const date = today.getDate();
 
-const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39];
+//目が悪くない人たちの席次
+const temporarySeats1 = [1, 2, 3, 4, 5, 6, 7, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 23, 24, 26, 27, 28, 30, 31, 32, 34, 35, 36, 37, 38];
 
-const shuffle = ([...array]) => {
-  for (let i = array.length - 1; i >= 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-}
-
-const fs = require('fs');
-const defaultFlexMessage = fs.readFileSync('flex-message.default.json','utf8');
-
-let flexMessage = defaultFlexMessage.replace('00', date);
-
-for( let i = 1; i >= 39; i++) {
-  flexMessage = flexMessage.replace(i + "?", shuffle[i])
+//とりあえずみんなの席をシャッフル。ここに目が悪い人の席を差し込んでいく
+for (let i1 = temporarySeats1.length - 1; i1 >= 0; i1--) {
+  const j1 = Math.floor(Math.random() * (i1 + 1));
+  [temporarySeats1[i1], temporarySeats1[j1]] = [temporarySeats1[j1], temporarySeats1[i1]];
 };
 
-fs.writeFileSync("flex-message.json", flexMessage);
+//目が悪い人の席をランダムに８つ選ぶ
+const array = [1, 2, 3, 8, 9, 10, 15, 16, 17, 21, 22, 23, 27, 28, 29, 34, 35, 36];
+const selected = [...Array(8)].map(() => array.splice(Math.floor(Math.random() * array.length), 1)[0]);
+
+//前の方から順にやっていかないと席がずれるので昇順でソート
+function compareFunc(a, b) {
+  return a - b;
+};
+
+selected.sort(compareFunc);
+
+//このままだと席次によって偏りが出るので目が悪い人の席次をシャッフル。さっきの席に当てはめるつもりで
+const temporarySeats2 = [8, 9, 10, 22, 25, 29, 33, 39];
+
+for (let i2 = temporarySeats2.length - 1; i2 >= 0; i2--) {
+  const j2 = Math.floor(Math.random() * (i2 + 1));
+  [temporarySeats2[i2], temporarySeats2[j2]] = [temporarySeats2[j2], temporarySeats2[i2]];
+};
+
+const changedSeats = (temporarySeats1, selected, temporarySeats2) => {
+  for (let i = 0; i < 8; i++) {
+    temporarySeats1.splice(selected[i] - 1, 0, temporarySeats2[i])
+  };
+  return temporarySeats1;
+};
+
+//２つのtemporarySeatsが合わさって席が決定。なんかもっとシンプルな方法ありそう
+changedSeats(temporarySeats1, selected, temporarySeats2);
+//ここまで席替え-----------------------------------------------------------------
+
+const fs = require('fs');
+
+const json = fs.readFileSync('flex-message.json', 'utf8');
+console.log(json);
 
 
 // -----------------------------------------------------------------------------
 // モジュールのインポート
+const server = require("express")();
 const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
 
 // -----------------------------------------------------------------------------
 // パラメータ設定
 const line_config = {
-    channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセットしています
-    channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
+    channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセット
+    channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセット
 };
+
+// -----------------------------------------------------------------------------
+// Webサーバー設定
+server.listen(process.env.PORT || 3000);
 
 // -----------------------------------------------------------------------------
 // APIコールのためのクライアントインスタンスを作成
@@ -40,12 +71,32 @@ const bot = new line.Client(line_config);
 
 // -----------------------------------------------------------------------------
 // ルーター設定
-const fs = require('fs');
-const flexMessage = fs.readFileSync("flex-message.json",'utf8');
+server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
+    // 先行してLINE側にステータスコード200でレスポンスする。
+    res.sendStatus(200);
 
-const message = FlexSendMessage.new_from_json_dict(flexMessage);
+    // すべてのイベント処理のプロミスを格納する配列。
+    let events_processed = [];
 
-bot.broadcast([
-  type: "flex",
-  text: message
-])
+    // イベントオブジェクトを順次処理。
+    req.body.events.forEach((event) => {
+        // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
+        if (event.type == "message" && event.message.type == "text"){
+            // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
+            if (event.message.text == "こんにちは"){
+                // replyMessage()で返信し、そのプロミスをevents_processedに追加。
+                events_processed.push(bot.replyMessage(event.replyToken, {
+                    type: "text",
+                    text: "これはこれは"
+                }));
+            }
+        }
+    });
+
+    // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
+    Promise.all(events_processed).then(
+        (response) => {
+            console.log(`${response.length} event(s) processed.`);
+        }
+    );
+});
