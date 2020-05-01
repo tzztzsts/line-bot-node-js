da5e4f984123767330aac5bb061d535717a19fc0
tzztzsts <tzztzsts@gmail.com>
2020-05-02 01:04:41 +0900

    Improve

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const flexMessageObj = require('./change.js');
 const {Client} = require('pg');
 
 const client = new Client({
-  user:  process.env.DB_USER,
+  user:  process.env.DB_USERNAME,
   host:  process.env.DB_HOST,
   database:  process.env.DB_DATABASE,
   password:  process.env.DB_PASSWORD,
